06aa7251eedf941dc53e48edcf7cea3bbb7f9ef9
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-11 12:29:16 +0200

    Thymeleaf Spring Boot

---
+++ src/main/java/com/zekerijah/templateengine/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.zekerijah.templateengine.controller;
+
+
+import com.zekerijah.templateengine.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("demo")
+    public String demo(Model model){
+        model.addAttribute("greeting", "Hello student");
+        model.addAttribute("message", "Exam result");
+
+        double grade = 90.5;
+
+        model.addAttribute("grade", grade);
+        model.addAttribute("GPA", convertGPA(grade));
+
+        return "demo";
+    }
+
+    @RequestMapping("demo2")
+    public String demo2(Model model) {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1, "Tom", 30));
+        userList.add(new User(2, "Jerry", 29));
+        userList.add(new User(3, "Nancy", 27));
+        model.addAttribute("list", userList);
+        return "demo2";
+    }
+
+    @RequestMapping("demo3")
+    public String demo3(HttpServletRequest request, Model model){
+        //Request
+        request.setAttribute("request", "request data");
+        //Session
+        request.getSession().setAttribute("session", "session data");
+        //Application
+        request.getSession().getServletContext().setAttribute("application", "application data");
+        return "demo2";
+    }
+
+    private String convertGPA(double grade){
+        if(grade>90) {
+            return "A";
+        } else if(grade<90 && grade>=80) {
+            return "B";
+        } else if(grade<80 && grade>=70) {
+            return "C";
+        } else if(grade<70 && grade>=60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
---
+++ src/test/java/com/zekerijah/templateengine/SpringBootTemplateEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekerijah.templateengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
